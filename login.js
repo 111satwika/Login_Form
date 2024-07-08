4523ecb2acee0068785eaacb2363abb585661fe7
Satwika Valumudi <136618119+111satwika@users.noreply.github.com>
2024-07-08 15:36:00 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+const linkEle=document.querySelector('.link');
+const formEle=document.querySelector('.form');
+
+linkEle.onclick=function(){
+    formEle.classList.toggle('show')
+}
+
+
+
+
+
---
+++ signup.js
@@ -0,0 +1,6 @@
+const linkEle=document.querySelector('.link');
+const formEle=document.querySelector('.form');
+
+linkEle.onclick=function(){
+    formEle.classList.toggle('show')
+}
